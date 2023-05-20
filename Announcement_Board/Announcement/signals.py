7f0dcb4704e6b2b190ccb648d77186786a2aec0d
posholokia <ilya.posholokk@gmail.com>
2023-05-20 23:11:27 +0600

    added: sending an email to the user when a response is accepted

--- Announcement_Board/Announcement/signals.py
+++ Announcement_Board/Announcement/signals.py
@@ -1,6 +1,6 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver  # импортируем нужный декоратор
-from .tasks import send_mail_response
+from .tasks import send_mail_response, send_mail_accept_resp
 from .models import ResponseToAnnounce
 
 
@@ -12,5 +12,15 @@ def notify_author_response(sender, instance, created, **kwargs):
         announce = instance.response_announcement.title
         email = [instance.response_announcement.author.email]
         send_mail_response.apply_async((instance.text, email, author, announce))
+        
+        
+@receiver(post_save, sender=ResponseToAnnounce)
+def notify_accept_response(sender, instance, created, **kwargs):
+    if not created and instance.accepted:
+        announce = instance.response_announcement.title
+        email = [instance.response_announcement.author.email]
+        pk = instance.response_announcement.id
+        username = instance.user.username
+        send_mail_accept_resp.apply_async((email, announce, pk, username))
 
     
--- Announcement_Board/Announcement/tasks.py
+++ Announcement_Board/Announcement/tasks.py
@@ -3,7 +3,7 @@ from celery import shared_task
 from Announcement_Board import settings
 from django.core.mail import EmailMultiAlternatives
 # import datetime
-# from news.models import Post, Category
+
 
 
 @shared_task
@@ -14,7 +14,7 @@ def send_mail_response(text, email, author, announce):
             'text': text[:49],
             'author': author,
             'announce': announce,
-            'link': f'{settings.SITE_URL}/board/my_announcement/responses'
+            'link': f'{settings.SITE_URL}/board/my_announcement/responses/'
         }
     )
     msg = EmailMultiAlternatives(
@@ -26,4 +26,23 @@ def send_mail_response(text, email, author, announce):
     msg.attach_alternative(html_content, "text/html")
     msg.send()
     
+
+@shared_task
+def send_mail_accept_resp(email, announce, pk, username):
+    html_content = render_to_string(
+        'send_mail_accept_resp.html',
+        {
+            'announce': announce,
+            'username': username,
+            'link': f'{settings.SITE_URL}/board/{pk}/'
+        }
+    )
+    msg = EmailMultiAlternatives(
+        subject='Новый отклик на Ваше объявление!',
+        body='',
+        from_email=settings.DEFAULT_FROM_EMAIL,
+        to=email,
+    )
+    msg.attach_alternative(html_content, "text/html")
+    msg.send()
     
